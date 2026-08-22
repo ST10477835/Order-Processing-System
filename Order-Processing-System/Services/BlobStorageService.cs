de19970448961077184dacc0ddd17643658bbadb
Abu <st10477835@myemeris.edu.za>
2026-08-22 21:52:02 +0200

    Blob Storage added for whenever a message is within a queue.

--- Order-Processing-System/Services/BlobStorageService.cs
+++ Order-Processing-System/Services/BlobStorageService.cs
@@ -1,4 +1,7 @@
 ﻿using Azure.Storage.Blobs;
+using Order_Processing_System.Models;
+using System.Text;
+using System.Text.Json;
 
 namespace Order_Processing_System.Services
 {
@@ -12,8 +15,18 @@ namespace Order_Processing_System.Services
             _blobServiceClient = new BlobServiceClient(
                 "UseDevelopmentStorage=true");
 
-            _blobContainerClient = _blobServiceClient.GetBlobContainerClient("Orders");
+            _blobContainerClient = _blobServiceClient.GetBlobContainerClient("orders");
             _blobContainerClient.CreateIfNotExists();
+        }
+
+        public async Task CreateBlobAsync(Order order)
+        {
+            BlobClient blob = _blobContainerClient.GetBlobClient($"order-{order.OrderId}");
+            string json = JsonSerializer.Serialize(order);
+            byte[] byteArray = Encoding.UTF8.GetBytes(json);
+            using MemoryStream stream = new MemoryStream(byteArray);
+
+            await blob.UploadAsync(stream, overwrite: true);
         }
     }
 }
--- Order-Processing-System/Workers/OrderProcessingWorker.cs
+++ Order-Processing-System/Workers/OrderProcessingWorker.cs
@@ -1,14 +1,19 @@
 ﻿using Order_Processing_System.Models;
 using Order_Processing_System.Services;
+using System.Text.Json;
 
 namespace Order_Processing_System.Workers
 {
     public class OrderProcessingWorker : BackgroundService
     {
         private readonly QueueStorageService _queueStorageService;
-        public OrderProcessingWorker(QueueStorageService queueStorageService)
+        private readonly BlobStorageService _blobStorageService;
+        public OrderProcessingWorker(
+            QueueStorageService queueStorageService,
+            BlobStorageService blobStorageService)
         {
             _queueStorageService = queueStorageService;
+            _blobStorageService = blobStorageService;
         }
         protected override async Task ExecuteAsync(CancellationToken stoppingToken)
         {
@@ -18,6 +23,12 @@ namespace Order_Processing_System.Workers
 
                 if (queueMessage != null)
                 {
+                    Order? order = JsonSerializer.Deserialize<Order>(queueMessage.MessageText);
+                    if(order != null)
+                    {
+                        await _blobStorageService.CreateBlobAsync(order);
+                        Console.WriteLine("Blob was created.");
+                    }
                     Console.WriteLine("Worker received: {0}", queueMessage.MessageText);
                     await _queueStorageService.DeleteMessageAsync(queueMessage.MessageId, queueMessage.PopReceipt);
                     Console.WriteLine("Message Deleted successfully");
